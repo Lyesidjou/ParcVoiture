169b20b7f211c5a785621022daf4bd37ce7e910d
Lyes Idjourdikane <lyes.idjourdikane@gmail.com>
2026-03-04 15:08:04 -0500

    creation de la classe Parc avec ses attributs et l'ajout de l'attribut liste_voitures qui va nous permettre de stocker les voitures a ajouter

--- parcvoiture.py
+++ parcvoiture.py
@@ -5,3 +5,10 @@ class Voiture:
         self.couleur = couleur
     def afficher_informations(self):
         print(f"la voiture immatriculé {self.matricule} de la marque {self.marque} et couleur {self.couleur}")
+class Parc:
+    def __init__(self, id:int , adresse : str, capacite : int):
+        self.id = id
+        self.adresse = adresse
+        self.capacite = capacite
+        self.liste_voitures=[]
+
